70ff99b6fd2e47585b20ff140ca83907bd232980
Aman-sahu-dev <amanempirex4k@gmail.com>
2024-06-13 18:26:34 +0530

    changed num1 value

--- src/main/java/com/slk/task/App.java
+++ src/main/java/com/slk/task/App.java
@@ -19,7 +19,7 @@ public class App
         Multiply mul = new Multiply();
         Divide div = new Divide();
         
-        int num1 = 15, num2 = 10;
+        int num1 = 150, num2 = 10;
         System.out.println("The operations performed on num1 and num2 is:\n");
         System.out.println("Add: " + add.add(num1, num2));
         System.out.println("Subtract: " + sub.subtract(num1, num2));
